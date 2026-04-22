6ccd2fae72b44674a7b2bd154863097b9c353288
hearingfuture70-wq <hearingfuture70@gmail.com>
2026-04-23 01:44:36 +0530

    Update route.ts

--- app/api/login /route.ts
+++ app/api/login /route.ts
@@ -1,16 +1,22 @@
 import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  const { username, password } = await req.json();
+  try {
+    const { username, password } = await req.json();
 
-  const result = await db.execute({
-    sql: "SELECT * FROM users WHERE username = ? AND password = ?",
-    args: [username, password],
-  });
+    const result = await db.execute({
+      sql: "SELECT * FROM users WHERE username = ? AND password = ?",
+      args: [username, password],
+    });
 
-  if (result.rows.length > 0) {
-    return Response.json({ success: true });
-  } else {
-    return Response.json({ success: false });
+    if (result.rows.length > 0) {
+      return NextResponse.json({ success: true });
+    } else {
+      return NextResponse.json({ success: false });
+    }
+
+  } catch (error) {
+    return NextResponse.json({ success: false });
   }
 }
