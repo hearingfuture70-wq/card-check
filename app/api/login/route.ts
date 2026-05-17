9b79f90478541c35f348a446fbc304bc0a01dc7f
hearingfuture70-wq <hearingfuture70@gmail.com>
2026-05-17 19:20:07 +0530

    route.ts

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,29 +1,19 @@
-import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
 
 export async function POST(req: Request) {
-
   try {
-
-    const body = await req.json();
-
-    console.log("BODY:", body);
-
-    const { username, password } = body;
+    const { username, password } = await req.json();
 
     const result = await db.execute({
       sql: "SELECT * FROM users WHERE username = ? AND password = ?",
-      args: [username.trim(), password.trim()],
+      args: [username, password],
     });
 
-    console.log("RESULT:", result.rows);
-
     if (result.rows.length > 0) {
-
       return NextResponse.json({
         success: true,
       });
-
     }
 
     return NextResponse.json({
@@ -31,14 +21,11 @@ export async function POST(req: Request) {
     });
 
   } catch (error) {
-
     console.log("LOGIN ERROR:", error);
 
     return NextResponse.json({
       success: false,
       error: String(error),
     });
-
   }
-
 }
